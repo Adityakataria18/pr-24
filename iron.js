849336242b055fe38008810775744e6bb4ddbb9b
Adityakataria18 <79639669+Adityakataria18@users.noreply.github.com>
2021-04-18 12:04:18 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,30 @@
+class Iron{
+	constructor(x,y,)
+	{
+	// assign options to the rubber ball
+	    var options={
+             restitution:0.8,
+			 friction:3,
+			 density:30
+		}
+        
+		this.x=x;
+		this.y=y;
+		
+		this.body=Bodies.rectangle(this.x, this.y, 50,50, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("black");
+			pop()
+	}
+
+}
---
+++ stone.js
@@ -0,0 +1,29 @@
+class Stone{
+	constructor(x,y,){
+         
+    var options={
+             restitution:0.8,
+			 friction:0.9,
+			 density:12
+		}
+        
+		this.x=x;
+		this.y=y;
+		
+		this.body=Bodies.rectangle(this.x, this.y, 50,50, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("red");
+			pop()
+	}
+
+}
